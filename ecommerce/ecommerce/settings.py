e7ef343a369964f2c7c7b667ca415fc1a90bbac6
its-shibu <shibusubba123@gmail.com>
2025-03-22 23:19:00 +0545

    'mysql_database_connection'

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "accounts",
+    "userspage",
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -83,8 +84,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online_shop',
+        'USER' : 'root',
+        'PASSWORD' : 'admin',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'"
+        }
     }
 }
 
