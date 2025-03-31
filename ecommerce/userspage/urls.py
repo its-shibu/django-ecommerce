ef4d6e48ee09ab3220d9c705456220bdc636888c
its-shibu <shibusubba123@gmail.com>
2025-04-01 00:11:36 +0545

    'add_to_cart'

--- ecommerce/userspage/urls.py
+++ ecommerce/userspage/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products),
-    path('productdetails/<int:product_id>', views.product_details)
+    path('productdetails/<int:product_id>', views.product_details),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name = 'add_to_cart'),
 ]
--- ecommerce/userspage/views.py
+++ ecommerce/userspage/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from demo_app.models import *
+from .models import *
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from accounts.auth import user_only
 
 
 def index(request):
@@ -22,3 +26,24 @@ def product_details(request, product_id):
         'product': product
     }
     return render(request, 'client/product_details.html', context)
+
+
+@login_required
+@user_only
+def add_to_cart(request, product_id):
+    user = request.user
+    product = Product.objects.get(id = product_id)
+    
+    check_itmes_presence = Cart.objects.filter(user=user, product= product)
+    if check_itmes_presence:
+        messages.add_message(request, messages.ERROR, 'Product already exist in the Cart.')
+        return redirect('/product')
+    else:
+        cart = Cart.objects.create(product=product, user=user)
+        if cart:
+            messages.add_message(request, messages.SUCCESS, 'Items added to cart')
+            return redirect('/mycart')
+        else:
+            messages.add_message(request, messages.ERROR, 'Unable to add to cart')
+            return redirect('/product')
+            
